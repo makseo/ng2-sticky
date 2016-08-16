dafc0ce0bcb3aaf2e4d02e71333cc154ffdcb778
Maksym Bidiuk <bidiukm@united-domains.de>
2016-08-16 11:52:02 +0200

    angular 2.0.0-rc.5

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,12 @@ module.exports = function(config) {
             // Reflect
             'node_modules/reflect-metadata/Reflect.js',
 
+            // Zone.js dependencies
+            'node_modules/zone.js/dist/zone.js',
+            'node_modules/zone.js/dist/jasmine-patch.js',
+            'node_modules/zone.js/dist/async-test.js',
+            'node_modules/zone.js/dist/fake-async-test.js',
+
             // RxJs
             {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false, served: true},
             {pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false, served: true},
